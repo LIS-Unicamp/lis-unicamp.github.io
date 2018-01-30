dcd9169db0ada508a3692843cc50a34a25e0e027
Lucas Carvalho <lucasaugustomcc@gmail.com>
2018-01-29 22:23:03 -0700

    alterar script de parser do arquivo bib

--- dev/bibparser.py
+++ dev/bibparser.py
@@ -1,43 +1,50 @@
+#-*- coding: utf-8 -*-
 import bibtexparser
 from bibtexparser.bparser import BibTexParser
 from bibtexparser.customization import homogeneize_latex_encoding
+from bibtexparser.customization import convert_to_unicode
 from bibtexparser.bwriter import BibTexWriter
 from bibtexparser.bibdatabase import BibDatabase
 
-def prep_list(list):
-	return "[" + ",".join(['"{0}"'.format(x) for x in list.split(", ")]) + "]"
+def prep_list(list, separator = ", "):
+	return "[" + ",".join(['"{0}"'.format(x) for x in list.split(separator)]) + "]"
+
+def write_file(bibtex_file):
+	list_quoting = ['year', 'date', 'link', 'booktitle', 'title', 'editor', 'abstract']
+
+	for item in list_quoting:
+		if item in bib.keys():
+			try:
+				bibtex_file.write("\n" + item + ": \"" + bib[item] + "\"")
+			except:
+				print bib['ID'] + ": " + item 
 
 with open('lis.bib') as bibtex_file:
     parser = BibTexParser()
     parser.customization = homogeneize_latex_encoding
+    #parser.customization = convert_to_unicode
     bibtex_database = bibtexparser.load(bibtex_file, parser=parser)
 
-writer = BibTexWriter()
-writer.indent = '    '
-
+i = 0
 for bib in bibtex_database.entries:
-	print (bib.keys())
 	name = bib['date']+'-'+bib['ID']+'.html'
-	with open("_posts/publications/" + name, 'w+') as bibtex_file:
+	with open("../_posts/publications/" + name, 'w+') as bibtex_file:
 		authors = bib['author'].split(" and ")
-		print authors
-		print authors[-1]
 		authors = "; ".join(["".join(x.split(" ")[-1]) + ", " + " ".join(x.split(" ")[0:-1]) for x in authors])
 
 		bibtex_file.write("---")
 		bibtex_file.write("\ncategory: publications")
-		bibtex_file.write("\nyear: " + bib['year'])
-		bibtex_file.write("\ndate: " + bib['date'])
-		bibtex_file.write("\ntitle: \"" + bib['title'] + "\"")
-		bibtex_file.write("\nlink: " + bib['link'])
 		bibtex_file.write("\ntype: " + bib['ENTRYTYPE'])
-		bibtex_file.write("\nauthors: " + prep_list(authors))
-		bibtex_file.write("\nbooktitle: \"" + bib['booktitle'] + "\"")
-		bibtex_file.write("\ntags: " + prep_list(bib['keyword'])) 
-		bibtex_file.write("\nabstract: \"" + bib['abstract'] + "\"")
+		bibtex_file.write("\nauthors: " + prep_list(authors, "; "))
+		bibtex_file.write("\ntags: " + prep_list(bib['keyword']))
+		write_file(bibtex_file) 		
 		bibtex_file.write("\n---")
 		bibtex_file.write("\n{% raw %}\n")
-		bibtexparser.dump(bibtex_database, bibtex_file)
+		db = BibDatabase()
+		db.entries = [bibtex_database.entries[i]]
+		try:
+			bibtexparser.dump(db, bibtex_file)
+		except:
+			print bib['ID']
 		bibtex_file.write("{% endraw %}")
-	exit()
-
+	i += 1
